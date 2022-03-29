24547db8123266bfd7be9365c874e4f8d751f579
François Beauducel <beauducel@gmail.com>
2022-03-29 10:59:31 +0200

    fix an issue when R=1

--- decim/decim.m
+++ decim/decim.m
@@ -1,7 +1,7 @@
 function y = decim(x,r,method)
 %DECIM	Fast data decimation.
 %	DECIM(X,R) resamples data in vector X at 1/R times the original sample
-%	rate, using the mean value of R consecutive samples. The resulting 
+%	rate, using the mean value of R consecutive samples. The resulting
 %	resampled vector is R times shorter. If X is a matrix, it returns the
 %	decimated vectors of each column.
 %
@@ -12,42 +12,42 @@ function y = decim(x,r,method)
 %	     'min' : minimum value
 %	     'max' : maximum value
 %
-%	Author: François Beauducel, IPGP
+%	Author: FranÃ§ois Beauducel, IPGP
 %	Created: 1996
-%	Updated: 2015-11-20
+%	Updated: 2022-03-29
 
-%	Copyright (c) 2015, François Beauducel, covered by BSD License.
+%	Copyright (c) 2021, FranÃ§ois Beauducel, covered by BSD License.
 %	All rights reserved.
 %
-%	Redistribution and use in source and binary forms, with or without 
-%	modification, are permitted provided that the following conditions are 
+%	Redistribution and use in source and binary forms, with or without
+%	modification, are permitted provided that the following conditions are
 %	met:
 %
-%	   * Redistributions of source code must retain the above copyright 
+%	   * Redistributions of source code must retain the above copyright
 %	     notice, this list of conditions and the following disclaimer.
-%	   * Redistributions in binary form must reproduce the above copyright 
-%	     notice, this list of conditions and the following disclaimer in 
-%	     the documentation and/or other materials provided with the 
+%	   * Redistributions in binary form must reproduce the above copyright
+%	     notice, this list of conditions and the following disclaimer in
+%	     the documentation and/or other materials provided with the
 %	     distribution
-%	                           
+%
 %	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
-%	IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED 
-%	TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A 
-%	PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT 
-%	OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
-%	SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
-%	LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
-%	DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY 
-%	THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
-%	(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+%	IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+%	TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+%	PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+%	OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+%	SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+%	LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+%	DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+%	THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+%	(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 %	OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 if nargin < 2
 	error('Not enough input arguments.')
 end
 
-if length(r) > 1 || r ~= round(r)
-	error('R must be an integer scalar!')
+if ~isscalar(r) || r ~= abs(round(r))
+	error('R must be an positive integer scalar!')
 end
 
 if nargin > 2
@@ -62,6 +62,12 @@ end
 if size(x,1) == 1
 	x = x';
 end
+
+if r == 1
+	y = x;
+	return
+end
+
 [m,n] = size(x);
 
 if m < r
